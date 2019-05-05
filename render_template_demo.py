b16811b1de094e2ece2330fb380f3feca3792ab1
Shuya Fuchigami <>
2019-05-05 15:07:05 +0200

    initial commit

---
+++ render_template_demo.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello(name=None):　#set default paramter: name=None
+    return render_template("hello.html", name=name) #このnameをhtml内で使用可能。
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
