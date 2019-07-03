029f0460d6b51423fbbf04180320cf2f3bd50c05
Shuya Fuchigami <>
2019-07-03 14:53:01 +0200

    change: do not show genre in table

--- static/scripts/player.js
+++ static/scripts/player.js
@@ -266,7 +266,6 @@ function changeToLoggedOutState() {
 }
 
 
-
 // if token is in cookie, you're logged in.
 async function hasToken() {
     if (getTokenFromCookie()) {
@@ -864,6 +863,7 @@ Object.defineProperty(this, 'displaySongList', {
         let tr = songSelector.insertRow(-1);
 
         for (const key of Object.keys(songTitle)) {  // with Object.keys() to get iterable keys. https://www.sejuku.net/blog/27965
+            if (key === "genre") continue;
             if (key === "created_at") continue; //continue: stop executing code below and continue to next loop;  break: stop executing rest of the loop
             tr.insertCell(-1).innerHTML = key;
         }
@@ -872,6 +872,7 @@ Object.defineProperty(this, 'displaySongList', {
         for (const song of songList) {
             let tr = songSelector.insertRow(-1);
             for (const [key, value] of Object.entries(song)) {
+                if (key === "genre") continue;
                 if (key === "created_at") continue;
                 tr.insertCell(-1).innerHTML = value;
             }
@@ -1705,7 +1706,7 @@ function getSongInfo(id) {
                     artist: tr.cells[2].innerText,
                     album: tr.cells[3].innerText,
                     year: tr.cells[4].innerText,
-                    genre: tr.cells[5].innerText
+                    // genre: tr.cells[5].innerText
                 }
             return songID;
         }
